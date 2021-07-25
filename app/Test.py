38a57cf88dd07b3141ff1abe644af16b9bb202db
farzane-mif <farzane-mif@users.noreply.github.com>
2021-07-25 12:42:26 +0200

    Update Test.py

--- app/Test.py
+++ app/Test.py
@@ -11,7 +11,7 @@ import sys
 module_path = os.path.abspath(os.path.join('../..'))
 if module_path not in sys.path:
     sys.path.append(module_path)
-from app.streamlit.SessionState import SessionState as session_state
+from SessionState import SessionState as session_state
 import base64
 
 
